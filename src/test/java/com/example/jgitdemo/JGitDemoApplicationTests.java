9a9f3a4d25f8db04fc78baef12ac1395ad39df68
Believer24 <18071688099@163.com>
2023-08-16 16:51:48 +0800

    dev test

--- src/test/java/com/example/jgitdemo/JGitDemoApplicationTests.java
+++ src/test/java/com/example/jgitdemo/JGitDemoApplicationTests.java
@@ -128,7 +128,7 @@ private static  UsernamePasswordCredentialsProvider provider =
 //            git.branchCreate().setName("dev").call();
 //            git.checkout().setName("dev").call();
             git.commit().setMessage("dev test").call();
-            git.push().add("dev").setCredentialsProvider( new UsernamePasswordCredentialsProvider("Believer24", "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")).call();
+            git.push().setRemote("origin").add("dev").setCredentialsProvider( new UsernamePasswordCredentialsProvider("Believer24", "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")).call();
         }
     }
 }
